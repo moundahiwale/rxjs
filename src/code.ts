fa20151b31d48802f4c6818aee365576fe65b1ce
moundahiwale <moundahiwale@yahoo.com>
2020-10-12 23:49:23 +0100

    Create observable and observer (subscription)

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,25 @@
-import * as Rx from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
+import { PartialObserver } from 'rxjs/Observer';
 
-console.log(Rx);
+const addItem = (val: string) => {
+  const node = document.createElement('li');
+  const textNode = document.createTextNode(val);
+  node.appendChild(textNode);
+  document.getElementById('output').appendChild(node);
+};
+
+const observable = Observable.create((observer: PartialObserver<string>) => {
+  try {
+    observer.next('Hello');
+    observer.complete();
+    observer.next('will not send');
+  } catch (error) {
+    observer.error(error);
+  }
+});
+
+observable.subscribe(
+  (success: string) => addItem(success),
+  (error: string) => addItem(error),
+  () => addItem('Completed')
+);
